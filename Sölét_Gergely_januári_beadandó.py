3d05c2f1abd53129656063a2cc5e4010fca845df
csuzli57 <83219803+csuzli57@users.noreply.github.com>
2022-01-16 20:44:22 +0100

    Add files via upload

--- Sölét_Gergely_januári_beadandó.py
+++ Sölét_Gergely_januári_beadandó.py
@@ -1,20 +1,12 @@
 import random
-ct="terrorelhárító" and "ct"
-t="terrorista" and "t"
-pénz=int(random.randint(200,5500))
-választás=1
-while választás!=ct and választás!=t:
-    választás=input("Válasszon egy csapatot (terrorelhárító vagy terrorista) ")
-if választás==ct:
-    print("terrorelhárítót választott")
-    print(str(pénz)+ "$-ja van")
+def ctvasarlas():
     ctshop = {
-  "usps": 200,
-  "five-seven": 500,
-  "desert eagle":700 ,
-  "ssg08":1700, 
-  "m4a1s": 3200,
-  "AWP":4750
+        "usps": 200,
+        "five-seven": 500,
+        "desert eagle":700 ,
+        "ssg08":1700, 
+        "m4a1s": 3200,
+        "AWP":4750
     }
     print(ctshop)
     vasarlas=input("Kérem irjon be egy fegyver nevét a fent láthatóak közül ")
@@ -27,16 +19,14 @@ if választás==ct:
             print("Nincsen elég pénze jöjjön vissza amikor már megvan az elegendő mennyiség")
         else:
             print(str(pénz2)+ "$-ja maradt")
-elif választás==t:
-    print("terroristát válaszott")
-    print(str(pénz)+ "$-ja van")
+def tvasarlas():
     tshop = {
-  "glock18":200,
-  "tec9": 500,
-  "desert eagle":700 ,
-  "ssg08":1700,
-  "AK47": 2700,
-  "AWP":4750
+        "glock18":200,
+        "tec9": 500,
+        "desert eagle":700 ,
+        "ssg08":1700,
+        "AK47": 2700,
+        "AWP":4750
     }
     print(tshop)
     vasarlas=input("Kérem irjon be egy fegyver nevét a fent láthatóak közül ")
@@ -49,3 +39,18 @@ elif választás==t:
             print("Nincsen elég pénze jöjjön vissza amikor már megvan az elegendő mennyiség")
         else:
             print(str(pénz2)+ "$-ja maradt")
+ct="terrorelhárító"
+t="terrorista" 
+pénz=int(random.randint(200,5500))
+választás=1
+
+while választás!=ct and választás!=t:
+    választás=input("Válasszon egy csapatot (terrorelhárító vagy terrorista) ")
+if választás==ct:
+    print("terrorelhárítót választott")
+    print(str(pénz)+ "$-ja van")
+    ctvasarlas()
+elif választás==t:
+    print("terroristát válaszott")
+    print(str(pénz)+ "$-ja van")
+    tvasarlas()
